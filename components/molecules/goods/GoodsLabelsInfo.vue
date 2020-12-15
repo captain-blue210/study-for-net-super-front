71ce1f6bb1e2ca8ae49cb0108c2565cc3561b6be
captain-blue210 <captain.blue210@gmail.com>
2020-12-15 22:22:36 +0900

    feat:#11 add catalog page

---
+++ components/molecules/goods/GoodsLabelsInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <label for>{{goodsName}}</label>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    goodsName: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
