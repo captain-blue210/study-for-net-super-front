eb1512c8e0e3f511be6b32495b880f4c7d428f10
captain-blue210 <captain.blue210@gmail.com>
2020-12-23 01:16:05 +0900

    style:pagination design

--- pages/search/Catalog.vue
+++ pages/search/Catalog.vue
@@ -1,17 +1,22 @@
 <template>
-  <section class="row-start-2 row-end-3 col-start-3 col-end-13 bg-yellow-200">
-    <div class="grid grid-rows-6 grid-cols-4">
+  <section class="row-start-2 row-end-3 col-start-3 col-end-13">
+    <div class="grid grid-cols-4">
       <CatalogItem :item="item" v-for="item in items" :key="item.id" />
+      <no-ssr>
+        <Paginate
+          class="col-start-1 col-end-5"
+          :container-class="'flex justify-center divide-x-2 divide-gray-500'"
+          :page-class="'border-2 border-t-2 border-b-2 py-1.5 px-3'"
+          :prev-class="'border-2 m-2'"
+          :next-class="'border-2 ml-8'"
+          :page-count="48"
+          :page-range="9"
+          :prevText="'前のページ'"
+          :nextText="'次のページ'"
+          :hide-prev-next="true"
+        />
+      </no-ssr>
     </div>
-    <no-ssr>
-      <Paginate
-        class="row-span-1 col-span-10 bg-green-200"
-        :page-count="48"
-        :page-range="9"
-        :prevText="'前のページ'"
-        :nextText="'次のページ'"
-      />
-    </no-ssr>
   </section>
 </template>
 
@@ -24,6 +29,13 @@ export default Vue.extend({
   name: 'Catalog',
   components: {
     CatalogItem,
+  },
+  data: function () {
+    return {
+      containerClass: {
+        'justify-between': true,
+      },
+    };
   },
   props: {
     items: {
