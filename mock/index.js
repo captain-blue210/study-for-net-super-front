2d9e6f0e487e83ec930bd701072a682ff2f6123d
Aoki Hayato <67002842+captain-blue210@users.noreply.github.com>
2020-12-28 18:54:37 +0900

    test:fix test data (#33)

---
+++ mock/index.js
@@ -0,0 +1,35 @@
+module.exports = () => {
+   const data = {
+      goods: []
+   }
+
+   // create descripions data
+   const mainDescriptionTxt = '商品メイン説明';
+   const subDescriptionTxt = '商品サブ説明';
+
+   const descripions = []
+
+   for (let i = 1; i <= 3; i++) {
+      descripions.push(
+         {
+            "description_id": i,
+            "description": i === 1 ? mainDescriptionTxt : `${subDescriptionTxt}${i - 1}`,
+            "description_type": i === 1 ? "main" : "sub"
+         },
+      )
+   }
+
+   for (let i = 1; i <= 50; i++) {
+      data.goods.push({
+         "id": 1,
+         "goodsName": `テスト商品${i}`,
+         "goodsBrand": "テストメーカー",
+         "saleUnit": "90g×2",
+         "goodsImgUri": "test2.jpg",
+         "excludingTaxPrice": 1000,
+         "includingTaxPrice": 1080,
+         "descriptions": descripions
+      })
+   }
+   return data;
+}
