14f6363fb0552509cdbf1b29364a7c0b13432a4b
captain-blue210 <captain.blue210@gmail.com>
2021-01-12 15:16:02 +0900

    feat:add going to order confirm button

---
+++ pages/step/Confirm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>注文確認画面</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<style scoped>
+</style>
--- pages/step/Payment.vue
+++ pages/step/Payment.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GoToOrderConfirmButton />
     <BackButton />
   </div>
 </template>
@@ -7,11 +8,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import BackButton from '~/components/molecules/common/button/BackButton.vue';
+import GoToOrderConfirmButton from '~/components/molecules/common/button/GoToOrderConfirmButton.vue';
 
 export default Vue.extend({
   name: 'Payment',
   components: {
     BackButton,
+    GoToOrderConfirmButton,
   },
 });
 </script>
