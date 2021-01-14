016f80f07834dbbfceca1ead5ef9dcc56949cd4c
captain-blue210 <captain.blue210@gmail.com>
2021-01-14 22:32:37 +0900

    feat:add order complete page

--- pages/step/Complete.vue
+++ pages/step/Complete.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>注文完了</div>
+  <div class="row-start-2 col-start-6 col-end-12 h-screen">
+    <AtomText :text="'注文完了しました'" :cssClass="'text-5xl'" />
+    <nuxt-link to="/">
+      <AtomText :text="'TOPへ'" :cssClass="'text-3xl ml-32 mt-20'" />
+    </nuxt-link>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import AtomText from '~/components/atoms/text/AtomText.vue';
 
 export default Vue.extend({
   name: 'Complete',
-  async asyncData({ store }) {
-    store.dispatch('completeOrder');
+  layout: 'cart',
+  components: {
+    AtomText,
   },
 });
 </script>
