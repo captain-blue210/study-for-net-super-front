e5fc48bf9cf57db4f029423252afb08c5c2644d3
captain-blue210 <captain.blue210@gmail.com>
2021-01-14 15:01:57 +0900

    feat:add payment method api mock

--- routes/api.ts
+++ routes/api.ts
@@ -3,9 +3,13 @@ export const ROUTES = {
     SEARCH: 'search',
     ITEM: 'item',
     CART: 'step/cart',
+    USER: 'v1/users',
   },
   POST: {
-    CART: 'cart/add',
+    CART: 'v1/cart',
+  },
+  PUT: {
+    CART: 'v1/cart',
   },
 };
 
